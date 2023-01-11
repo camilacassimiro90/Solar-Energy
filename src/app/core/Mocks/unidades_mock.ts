f67942b37ba5f8733c423b143d228cc90cd6947a
Camila Cassimiro <camilacassimiro@gmail.com>
2023-01-11 17:10:40 -0300

    fix pastas

--- src/app/core/Mocks/unidades_mock.ts
+++ src/app/core/Mocks/unidades_mock.ts
@@ -1,36 +1,36 @@
-import { IUnidade } from "../shared/unidades";
+import { IUnidade } from "../shared/models/unidades";
 
-export const MOCK_UNIDADES :IUnidade[] = [
-  {
-    apelido : 'Painel 1',
-    local : 'Rua 5',
-    marca : 'Canadian',
-    modelo : '155w',
-    isActive: false,
-    id : 1,
-},
-{
-    apelido : 'Painel 2',
-    local : 'Rua 15',
-    marca : 'Risen',
-    modelo : '100w',
-    isActive: false,
-    id : 2,
-},
-{
-    apelido : 'Painel 3',
-    local : 'Rio de Janeiro',
-    marca : 'Hanwha',
-    modelo : '200w',
-    isActive: false,
-    id : 3,
-},
-{
-    apelido : 'Painel 4',
-    local : 'São Paulo',
-    marca : 'Hanwha',
-    modelo : '100w',
-    isActive: false,
-    id : 4,
-},
+export const MOCK_UNIDADES: IUnidade[] = [
+    {
+        apelido: 'Painel 1',
+        local: 'Rua 5',
+        marca: 'Canadian',
+        modelo: '155w',
+        isActive: false,
+        id: 1,
+    },
+    {
+        apelido: 'Painel 2',
+        local: 'Rua 15',
+        marca: 'Risen',
+        modelo: '100w',
+        isActive: false,
+        id: 2,
+    },
+    {
+        apelido: 'Painel 3',
+        local: 'Rio de Janeiro',
+        marca: 'Hanwha',
+        modelo: '200w',
+        isActive: false,
+        id: 3,
+    },
+    {
+        apelido: 'Painel 4',
+        local: 'São Paulo',
+        marca: 'Hanwha',
+        modelo: '100w',
+        isActive: false,
+        id: 4,
+    },
 ]
